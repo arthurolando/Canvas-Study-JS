71aaab0b8272838da2651ae112c5efdfb8252d9f
Arthur Sampaio <arthursampaio1611@gmail.com>
2023-09-04 22:04:32 -0300

    AccelerationShip

--- 08-ExplosionOfBalls/main.js
+++ 08-ExplosionOfBalls/main.js
@@ -5,10 +5,18 @@ let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
 
 var particles = [],
-    numParticles = 100;
+    numParticles = 100,
+    velocity,
+    direction,
+    gravity = 0.0;
+
+document.querySelector("#debug span#numBalls").innerText = `Particles:(${numParticles})`;
+document.querySelector("#debug span#gravity").innerText = `gravity:(${gravity})`;
 
 for (var i = 0; i < numParticles; i++) {
-    particles.push(particle.create(width / 2, height / 2, Math.random() * 4 + 1, Math.random() * Math.PI * 2));
+    velocity =  Math.random() * 5 + 2;
+    direction = Math.random() * Math.PI * 2;
+    particles.push(particle.create(width / 2, height / 2, velocity, direction, gravity));
 }
 
 function init() {
@@ -20,9 +28,19 @@ function resetResize() {
     height = canvas.height = window.innerHeight; 
 }
 
+console.log("To change gravity use changeGravity(value)");
+function changeGravity(gravity) {
+    gravity = gravity
+    for (var i = 0; i < numParticles; i++) {
+        particles[i].gravity = vector.create(0, gravity);
+    }
+    document.querySelector("#debug span#gravity").innerText = `gravity:(${gravity})`;
+}
+
 function Draw(p) {
     context.beginPath();
     context.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 2, false);
+    context.fillStyle = "black";
     context.fill();
     context.closePath();
 }
@@ -40,9 +58,12 @@ function animationLoop() {
 
 function click(e) {
     for (var i = 0; i < numParticles; i++) {
+        velocity =  Math.random() * 5 + 2;
         var p = particles[i];
         p.position.setX(e.x);
         p.position.setY(e.y);
+        p.velocity.setLength(velocity);
+        p.velocity.setAngle(Math.random() * Math.PI * 2);
         p.update();
         Draw(p);
     }
